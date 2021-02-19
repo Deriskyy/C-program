4dae49489d48142eaa0ee86bb8d25e8a231e299f
Deriskyy <43159171+Deriskyy@users.noreply.github.com>
2021-02-19 19:01:12 +0800

    Add files via upload

---
+++ FibonacciNumber.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int Fib(int n);
+{
+    if (n<=2)
+        printf("The number should be larger than 2.\n");
+    else
+        return Fib(n-1) + Fib(n-2);
+}
+
+int main()
+{    
+   int num;
+   printf("Which one Fibonacci number do you want to get?\n");
+   scanf("%d",&num);
+   
+   int i
+   for (i=0,i<n+1;i++);
+   
+   printf("The &d_th Fibonacci number is %d\n",num,Fib(num));
+   retunr 0;
+}   
+   
